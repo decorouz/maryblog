7e54c5361c7be2e2a247ee1312e415f155dee208
Biola <52200708+decorouz@users.noreply.github.com>
2021-12-31 20:24:27 +0100

    Create basic search engine

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,15 @@
+from django.contrib.postgres import search
 from django.core import paginator
+from django.db.models.fields.related import OneToOneField
 from django.shortcuts import render, get_object_or_404
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-from blog.forms import EmailPostForm, CommentForm
+from blog.forms import EmailPostForm, CommentForm, SearchForm
 from .models import Post, Comment
 from django.views.generic import ListView
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
 
 
 # class PostListView(ListView):
@@ -97,3 +100,19 @@ def post_detail(request, year, month, day, post):
                                                      "new_comment": new_comment,
                                                      "comment_form": comment_form,
                                                      "similar_posts": similar_posts})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if "query" in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            results = Post.published.annotate(
+                search=SearchVector("title", "body"),).filter(search=query)
+    return render(request, "blog/post/search.html",
+                  {"form": form,
+                   "query": query,
+                   "results": results})
