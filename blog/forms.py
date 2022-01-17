7bd327fadcf69e45df97d1810eba748cbd15f5f0
Biola <52200708+decorouz@users.noreply.github.com>
2022-01-17 17:41:28 +0100

    Update comment widget

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,8 @@
+# Core Django imports
+from django.forms import ModelForm, TextInput, EmailInput, Textarea
 from django import forms
 
+# Blog application imports
 from blog.models import Comment
 
 
@@ -10,12 +13,36 @@ class EmailPostForm(forms.Form):
     comments = forms.CharField(required=False, widget=forms.Textarea)
 
 
-class CommentForm(forms.ModelForm):
+class CommentForm(ModelForm):
     """Creating form from Comment model"""
     class Meta:
         model = Comment
         fields = ("name", "email", "body")
 
+        widgets = {
+            "name": TextInput(attrs={"aria-required": "true",
+                                     "name": "contact-form-name",
+                                     "class": "form-control",
+                                     "placeholder": "Enter your name",
+                                     "aria-valid": "true"}),
+
+            "email": EmailInput(attrs={"aria-required": "true",
+                                       "name": "contact-form-email",
+                                       "class": "form-control",
+                                       "placeholder": "Enter your email",
+                                       "aria-valid": "true"}),
+
+            "body": Textarea(attrs={"row": 2,
+                                    "name": "contact-form-message",
+                                    "class": "text-area-message form-control",
+                                    "placeholder": "Enter your email",
+                                    "arai-required": "true",
+                                    "aria-valid": "false"})
+        }
+
 
 class SearchForm(forms.Form):
     query = forms.CharField()
+
+
+#
